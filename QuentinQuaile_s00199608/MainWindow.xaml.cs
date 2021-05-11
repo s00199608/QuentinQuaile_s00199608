8b7e3040702598a87d6ccce520e5157df2e68200
Quentin Quaile <S00199608@mail.itsligo.ie>
2021-05-11 12:41:41 +0100

    Filter List

--- QuentinQuaile_s00199608/MainWindow.xaml.cs
+++ QuentinQuaile_s00199608/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace QuentinQuaile_s00199608
     public partial class MainWindow : Window
     {
         List<Game> GameList = new List<Game>();
+        List<Game> FilterGameList = new List<Game>();
         static GameData db = new GameData();
         public MainWindow()
         {
@@ -32,8 +33,67 @@ namespace QuentinQuaile_s00199608
             var query = from games in db.Games
                         select games;
 
-            GameBox.ItemsSource = query.ToList();
+            GameList = query.ToList();
+            GameBox.ItemsSource = GameList;
                         
+        }
+
+        private void GameBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Game selected = GameBox.SelectedItem as Game;
+
+            if(selected != null)
+            {
+                DescriptionBlock.Text = selected.Description;
+
+                GamerImage.Source = new BitmapImage(new Uri(selected.GameImage, UriKind.Relative));
+            }
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            GameBox.ItemsSource = null;
+            GameBox.ItemsSource = GameList;
+        }
+
+        private void radioButton_Copy_Checked(object sender, RoutedEventArgs e)
+        {
+            FilterGameList = null;
+
+            var query = from games in db.Games
+                        where games.Platform.Contains("Xbox")
+                        select games;
+
+            FilterGameList = query.ToList();
+
+            GameBox.ItemsSource = null;
+            GameBox.ItemsSource = FilterGameList;
+        }
+
+        private void radioButton_Copy1_Checked(object sender, RoutedEventArgs e)
+        {
+            FilterGameList = null;
+
+            var query = from games in db.Games
+                        where games.Platform.Contains("PS")
+                        select games;
+            FilterGameList = query.ToList();
+
+            GameBox.ItemsSource = null;
+            GameBox.ItemsSource = FilterGameList;
+        }
+
+        private void radioButton_Copy2_Checked(object sender, RoutedEventArgs e)
+        {
+            FilterGameList = null;
+
+            var query = from games in db.Games
+                        where games.Platform.Contains("PC")
+                        select games;
+            FilterGameList = query.ToList();
+
+            GameBox.ItemsSource = null;
+            GameBox.ItemsSource = FilterGameList;
         }
     }
 }
